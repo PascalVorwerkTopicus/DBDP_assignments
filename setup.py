d72ef042892dc5a63a049298962416ef7b3cd5a1
Claudien <478229@student.saxion.nl>
2022-09-23 13:47:54 +0200

    Added init files in each directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DBDP assignments',
+    version='0.0',
+    packages=find_packages(),
+    install_requires=[ 
+        "termcolor==1.1.0",        
+    ]
+)
